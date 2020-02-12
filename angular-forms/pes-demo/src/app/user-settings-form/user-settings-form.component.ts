1ad448e3e44059847ea3a7e6e40ea801f2830eea
Usama Ansari <usansa@microsoft.com>
2020-02-12 11:16:57 +0530

    moved filter to right

--- angular-forms/pes-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ angular-forms/pes-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from './data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: 'Usama',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'Here are some notes'
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings }
 
   constructor() { }
 
